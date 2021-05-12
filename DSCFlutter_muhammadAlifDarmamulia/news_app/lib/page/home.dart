a2c60cc5838a5ef7d1210380ca07dbf09849e0f2
MalifD <59762698+alif338@users.noreply.github.com>
2021-05-12 14:02:23 +0700

    Add files via upload

---
+++ DSCFlutter_muhammadAlifDarmamulia/news_app/lib/page/home.dart
@@ -0,0 +1,169 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/page/news_item_techno.dart';
+import 'package:news_app/page/news_item_trend.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'news_model.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String apiUrlTrend = "https://innocent-dscitb.vercel.app/trend.json";
+  String apiUrlTechno = "https://innocent-dscitb.vercel.app/news.json";
+  News trendNews;
+  News technoNews;
+  bool isLoading = true;
+
+  var dio = new Dio();
+
+  @override
+  void initState() {
+    super.initState();
+    // getTrendNews();
+  }
+
+  Future<void> _launchInBrowser(String url) async {
+    await canLaunch(url).then((value) async {
+      if (value) {
+        await launch(
+          url,
+          forceSafariVC: true,
+          forceWebView: true,
+          headers: <String, String>{'my_header_key': 'my_header_value'},
+        );
+      } else {
+        throw 'Cannot launch $url';
+      }
+    }).catchError((onError) {
+      print(onError);
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("MNC News App", style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white,
+        elevation: 1,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.black,),
+            onPressed: (){}
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        physics: ScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 16, right: 16,top: 8),
+                child: Text(
+                  "Trending News",
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                      fontSize: 21,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 200,
+              child: FutureBuilder(
+                future: dio.get(apiUrlTrend),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final trendResponse = News.fromJson(snapshot.data.data);
+                    return ListView.builder(
+                      itemCount: trendResponse.data.length,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context,index){
+                        return InkWell(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(8,8,8,0),
+                              child: NewsItemTrend(news: trendResponse.data[index],),
+                            ),
+                          onTap: () => _launchInBrowser(trendResponse.data[index].url),
+                        );
+                      }
+                    );
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+
+              ),
+            ),
+            SizedBox(height: 15,),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 16),
+                child: Text(
+                  "Techno News",
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                        fontSize: 21,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold
+                    )
+                  ),
+                ),
+              ),
+            ),
+            FutureBuilder(
+              future: dio.get(apiUrlTechno),
+              builder: (context, snapshot) {
+                if (snapshot.hasData){
+                  final arr = snapshot.data;
+                  print(arr);
+                  final technoResponse = News.fromJson(snapshot.data.data);
+                  return ListView.builder(
+                    itemCount: technoResponse.data.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    physics: ScrollPhysics(),
+                    itemBuilder: (context,index){
+                      return InkWell(
+                        child: Container(
+                          width: double.infinity,
+                          height: 135,
+                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 8),
+                          child: NewsItemTechno(news: technoResponse.data[index],),
+                        ),
+                        onTap: () => _launchInBrowser(technoResponse.data[index].url),
+                      );
+                    }
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator(),);
+                }
+              }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ DSCFlutter_muhammadAlifDarmamulia/news_app/lib/page/news_item_techno.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'news_model.dart';
+
+class NewsItemTechno extends StatelessWidget {
+
+  final Datum news;
+
+  const NewsItemTechno({
+    Key key,
+    this.news}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: Colors.grey.shade500, width: 1),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 90,
+            height: 135,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: NetworkImage(news.thumbnail),
+                fit: BoxFit.cover
+              ),
+            ),
+          ),
+          SizedBox(height: 12,),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    news.title,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 4,),
+                  Text(
+                    news.summary,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black54
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    news.published,
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey
+                      ),
+                    )
+                  )
+                ],
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
