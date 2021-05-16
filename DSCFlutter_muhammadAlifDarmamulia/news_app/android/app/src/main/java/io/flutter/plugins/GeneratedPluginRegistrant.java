417369e4da56ff29b8b97619fc1e5878928da774
MalifD <59762698+alif338@users.noreply.github.com>
2021-05-16 09:42:27 +0700

    Add files via upload

--- DSCFlutter_muhammadAlifDarmamulia/news_app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ DSCFlutter_muhammadAlifDarmamulia/news_app/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -13,7 +13,10 @@ import io.flutter.embedding.engine.FlutterEngine;
 @Keep
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    flutterEngine.getPlugins().add(new io.flutter.plugins.androidalarmmanager.AndroidAlarmManagerPlugin());
+    flutterEngine.getPlugins().add(new com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
+    flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.urllauncher.UrlLauncherPlugin());
   }
 }
